7bf8024c4ed729b87a0574b2e001061be32fc2a0
YouheiNozaki <youhey.hey0505@gmail.com>
2022-01-14 17:10:58 +0900

    add menu

--- src/TipTap.tsx
+++ src/TipTap.tsx
@@ -1,17 +1,85 @@
-import { useEditor, EditorContent } from '@tiptap/react';
+import {
+  useEditor,
+  EditorContent,
+  BubbleMenu,
+  FloatingMenu,
+} from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
+import Highlight from '@tiptap/extension-highlight';
+import Typography from '@tiptap/extension-typography';
 import './styles.scss';
 
 import { MenuBar } from './Mebubar';
 
 export const Tiptap = () => {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [StarterKit, Highlight, Typography],
     content: '<p>Hello World!</p>',
   });
 
   return (
     <div>
+      {editor && (
+        <BubbleMenu
+          className="bubble-menu"
+          tippyOptions={{ duration: 100 }}
+          editor={editor}
+        >
+          <button
+            onClick={() => editor.chain().focus().toggleBold().run()}
+            className={editor.isActive('bold') ? 'is-active' : ''}
+          >
+            Bold
+          </button>
+          <button
+            onClick={() => editor.chain().focus().toggleItalic().run()}
+            className={editor.isActive('italic') ? 'is-active' : ''}
+          >
+            Italic
+          </button>
+          <button
+            onClick={() => editor.chain().focus().toggleStrike().run()}
+            className={editor.isActive('strike') ? 'is-active' : ''}
+          >
+            Strike
+          </button>
+        </BubbleMenu>
+      )}
+
+      {editor && (
+        <FloatingMenu
+          className="floating-menu"
+          tippyOptions={{ duration: 100 }}
+          editor={editor}
+        >
+          <button
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 1 }).run()
+            }
+            className={
+              editor.isActive('heading', { level: 1 }) ? 'is-active' : ''
+            }
+          >
+            H1
+          </button>
+          <button
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 2 }).run()
+            }
+            className={
+              editor.isActive('heading', { level: 2 }) ? 'is-active' : ''
+            }
+          >
+            H2
+          </button>
+          <button
+            onClick={() => editor.chain().focus().toggleBulletList().run()}
+            className={editor.isActive('bulletList') ? 'is-active' : ''}
+          >
+            Bullet List
+          </button>
+        </FloatingMenu>
+      )}
       <MenuBar editor={editor} />
       <EditorContent editor={editor} />
     </div>
