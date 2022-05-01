f7141954c78c6aaf7014409a1d81f97a9c7b2b87
Youhei Nozaki <youhey.hey0505@gmail.com>
2022-05-01 18:10:16 +0900

    fix heading

--- src/components/RichEditor2/MenuBar/Heading/index.tsx
+++ src/components/RichEditor2/MenuBar/Heading/index.tsx
@@ -20,95 +20,99 @@ export const Heading: React.VFC<Props> = ({ editor }) => {
 
   return (
     <Group>
-      <Tooltip
-        trigger={
-          <Button
-            text={
-              isHeading1
-                ? '見出し1'
-                : isHeading2
-                ? '見出し2'
-                : isHeading3
-                ? '見出し3'
-                : isHeading4
-                ? '見出し4'
-                : isHeading5
-                ? '見出し5'
-                : '段落'
-            }
-            icon={<span className="material-icons-outlined">unfold_more</span>}
-          />
-        }
-      >
-        <ul className={styles.list}>
-          <li
-            onClick={() => {
-              editor.chain().focus().toggleHeading({ level: 1 }).run();
-            }}
-            className={cx(styles.li, {
-              [styles.h1]: true,
-              [styles.isActive]: isHeading1,
-            })}
-          >
-            見出し1
-          </li>
-          <li
-            onClick={() => {
-              editor.chain().focus().toggleHeading({ level: 2 }).run();
-            }}
-            className={cx(styles.li, {
-              [styles.h2]: true,
-              [styles.isActive]: isHeading2,
-            })}
-          >
-            見出し2
-          </li>
-          <li
-            onClick={() => {
-              editor.chain().focus().toggleHeading({ level: 3 }).run();
-            }}
-            className={cx(styles.li, {
-              [styles.h3]: true,
-              [styles.isActive]: isHeading3,
-            })}
-          >
-            見出し3
-          </li>
-          <li
-            onClick={() => {
-              editor.chain().focus().toggleHeading({ level: 4 }).run();
-            }}
-            className={cx(styles.li, {
-              [styles.h4]: true,
-              [styles.isActive]: isHeading4,
-            })}
-          >
-            見出し4
-          </li>
-          <li
-            onClick={() => {
-              editor.chain().focus().toggleHeading({ level: 5 }).run();
-            }}
-            className={cx(styles.li, {
-              [styles.h5]: true,
-              [styles.isActive]: isHeading5,
-            })}
-          >
-            見出し5
-          </li>
-          <li
-            onClick={() => {
-              editor.commands.setParagraph();
-            }}
-            className={cx(styles.li, {
-              [styles.paragrah]: true,
-              [styles.isActive]: editor.isActive('paragraph'),
-            })}
-          >
-            段落
-          </li>
-        </ul>
-      </Tooltip>
+      <div className={styles.wrapper}>
+        <Tooltip
+          trigger={
+            <Button
+              text={
+                isHeading1
+                  ? '見出し1'
+                  : isHeading2
+                  ? '見出し2'
+                  : isHeading3
+                  ? '見出し3'
+                  : isHeading4
+                  ? '見出し4'
+                  : isHeading5
+                  ? '見出し5'
+                  : '段落'
+              }
+              icon={
+                <span className="material-icons-outlined">unfold_more</span>
+              }
+            />
+          }
+        >
+          <ul className={styles.list}>
+            <li
+              onClick={() => {
+                editor.chain().focus().toggleHeading({ level: 1 }).run();
+              }}
+              className={cx(styles.li, {
+                [styles.h1]: true,
+                [styles.isActive]: isHeading1,
+              })}
+            >
+              見出し1
+            </li>
+            <li
+              onClick={() => {
+                editor.chain().focus().toggleHeading({ level: 2 }).run();
+              }}
+              className={cx(styles.li, {
+                [styles.h2]: true,
+                [styles.isActive]: isHeading2,
+              })}
+            >
+              見出し2
+            </li>
+            <li
+              onClick={() => {
+                editor.chain().focus().toggleHeading({ level: 3 }).run();
+              }}
+              className={cx(styles.li, {
+                [styles.h3]: true,
+                [styles.isActive]: isHeading3,
+              })}
+            >
+              見出し3
+            </li>
+            <li
+              onClick={() => {
+                editor.chain().focus().toggleHeading({ level: 4 }).run();
+              }}
+              className={cx(styles.li, {
+                [styles.h4]: true,
+                [styles.isActive]: isHeading4,
+              })}
+            >
+              見出し4
+            </li>
+            <li
+              onClick={() => {
+                editor.chain().focus().toggleHeading({ level: 5 }).run();
+              }}
+              className={cx(styles.li, {
+                [styles.h5]: true,
+                [styles.isActive]: isHeading5,
+              })}
+            >
+              見出し5
+            </li>
+            <li
+              onClick={() => {
+                editor.commands.setParagraph();
+              }}
+              className={cx(styles.li, {
+                [styles.paragrah]: true,
+                [styles.isActive]: editor.isActive('paragraph'),
+              })}
+            >
+              段落
+            </li>
+          </ul>
+        </Tooltip>
+      </div>
     </Group>
   );
 };
